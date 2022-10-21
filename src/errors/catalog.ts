146648c1b3c2d8394d63564930d0803682c51fed
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-21 17:26:48 -0300

    refactor: acrescenta ao catálogo de erros o erro Entity not found

--- src/errors/catalog.ts
+++ src/errors/catalog.ts
@@ -1,5 +1,6 @@
 export enum ErrorTypes {
   InvalidMongoId = 'InvalidMongoId',
+  EntityNotFound = 'EntityNotFound',
 }
 
 type ErrorResponseObject = { 
@@ -13,5 +14,9 @@ export const errorCatalog: ErrorCatalog = {
   InvalidMongoId: {
     message: 'Id must be a 24 characters hexadecimal',
     httpStatus: 400,
+  },
+  EntityNotFound: {
+    message: 'Entity Not Found',
+    httpStatus: 404,
   },
 };
