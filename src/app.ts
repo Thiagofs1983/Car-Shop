6804d8870a52dad1b1ad65825d0ca5888b95a2f8
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-21 17:59:42 -0300

    feat: importa rota e middlerware de erro no app e usa os mesmos

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import errorHandler from './middlewares/error';
+import carRoute from './routes/Cars';
 
 const app = express();
+
+app.use(express.json());
+app.use(carRoute);
+app.use(errorHandler);
 
 export default app;
