77eac83cf3c64a76cc153eea53a8cc56a606541d
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-21 16:51:45 -0300

    feat: catalogo de erros

---
+++ src/errors/catalog.ts
@@ -0,0 +1,17 @@
+export enum ErrorTypes {
+  InvalidMongoId = 'InvalidMongoId',
+}
+
+type ErrorResponseObject = { 
+  message: string;
+  httpStatus: number
+};
+
+export type ErrorCatalog = Record<ErrorTypes, ErrorResponseObject>;
+
+export const errorCatalog: ErrorCatalog = {
+  InvalidMongoId: {
+    message: 'Id must be a 24 characters hexadecimal',
+    httpStatus: 400,
+  },
+};
