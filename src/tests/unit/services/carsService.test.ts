3adab6cc3a49da5f5826edefd9c50f128d5f1adc
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-22 06:29:12 -0300

    test: teste para verificar se gera um erro caso os dados enviados não estejam corretos

--- src/tests/unit/services/carsService.test.ts
+++ src/tests/unit/services/carsService.test.ts
@@ -3,6 +3,7 @@ import chai from 'chai';
 import CarsModel from '../../../models/Cars';
 import CarsService from '../../../services/Cars';
 import { mockNewCar, mockSendNewCar } from '../../mocks/carsMock';
+import { ZodError } from 'zod';
 const { expect } = chai;
 
 describe('Car Service', () => {
@@ -21,6 +22,15 @@ describe('Car Service', () => {
     it('Caso de sucesso', async () => {
       const carCreated = await carsService.create(mockSendNewCar);
       expect(carCreated).to.be.deep.equal(mockNewCar);
+    });
+    it('Caso os dados não sejam enviados corretamente gera um ERRO', async () => {
+      let error;
+      try {
+        await carsService.create({});
+      } catch(err) {
+        error = err;
+      }
+      expect(error).to.be.instanceOf(ZodError);
     });
   });
 });
