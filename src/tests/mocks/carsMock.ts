5e3fbcc5c1990b3be8ee5d13200a3a4f1c990fd0
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-22 06:12:28 -0300

    test: cria pasta de mocks reaproveitar os mocks feitos para a camada model, na camada service

---
+++ src/tests/mocks/carsMock.ts
@@ -0,0 +1,19 @@
+export const mockNewCar = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const mockSendNewCar = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2
+};
