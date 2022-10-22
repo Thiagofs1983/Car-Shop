616cd20023af74ad7c63fef15ed8c5c8818d622d
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-22 12:25:52 -0300

    fix: substitui a string /motorcycles/:id nas rotas para evitar repetições

--- src/routes/Motorcycle.ts
+++ src/routes/Motorcycle.ts
@@ -5,14 +5,15 @@ import MotorcycleService from '../services/Motorcycle';
 
 const route = Router();
 
+const motorcycleId = '/motorcycles/:id';
 const motorcycle = new MotorcycleModel();
 const motorcycleService = new MotorcycleService(motorcycle);
 const motorcycleController = new MotorcycleController(motorcycleService);
 
 route.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
 route.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
-route.get('/motorcycles/:id', (req, res) => motorcycleController.readOne(req, res));
-route.put('/motorcycles/:id', (req, res) => motorcycleController.update(req, res));
-route.delete('/motorcycles/:id', (req, res) => motorcycleController.delete(req, res));
+route.get(motorcycleId, (req, res) => motorcycleController.readOne(req, res));
+route.put(motorcycleId, (req, res) => motorcycleController.update(req, res));
+route.delete(motorcycleId, (req, res) => motorcycleController.delete(req, res));
 
 export default route;
