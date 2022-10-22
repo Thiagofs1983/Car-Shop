ae32338859a0319ee8100a942fd54dfd62676263
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-10-22 08:15:11 -0300

    test: acrescenta mocks para atualizar um carro e refatora os mocks para diminuir linhas

--- src/tests/mocks/carsMock.ts
+++ src/tests/mocks/carsMock.ts
@@ -1,5 +1,4 @@
-export const mockNewCar = {
-  _id: "4edd40c86762e0fb12000003",
+export const mockSendNewCar = {
   model: "Ferrari Maranello",
   year: 1963,
   color: "red",
@@ -8,12 +7,21 @@ export const mockNewCar = {
   doorsQty: 2
 };
 
-export const mockSendNewCar = {
+export const mockNewCar = {
   _id: "4edd40c86762e0fb12000003",
-  model: "Ferrari Maranello",
-  year: 1963,
-  color: "red",
-  buyValue: 3500000,
+  ...mockSendNewCar,
+};
+
+export const mockSendUpdateCar = {
+  model: "Mazzerati Turbo",
+  year: 2004,
+  color: "blue",
+  buyValue: 600000,
   seatsQty: 2,
   doorsQty: 2
+};
+
+export const mockUpdateCar = {
+  _id: "4edd40c86762e0fb12000003",
+  ...mockSendUpdateCar,
 };
